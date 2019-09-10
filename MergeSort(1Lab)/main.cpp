3e9978dc715dac3e80b94e6b55d60c6377a36a85
Vitaliy Datsiuk <vetalvetal222@gmail.com>
2019-09-10 18:22:06 +0300

    Final version of my Lab
    Done the merging sort, using auxillary files and mergesort for small pieces of data.
    
    10.09.2019

--- MergeSort(1Lab)/main.cpp
+++ MergeSort(1Lab)/main.cpp
@@ -1,33 +1,53 @@
 #include <iostream>
 #include <stdio.h>
-#include <vector>
 #include <string>
-#define deflen 10
-int files_num=0;
+#define numfiles 20                     //Number files for dividing on blocks
+#define rand_num 20000                    //Number of random created integers in main source file(inA)
+int deflen=rand_num/numfiles;           //Number of integers in every file
+int files_num=0;                        //Counter for num of files
 
 using namespace std;
 
-FILE *fouta=fopen("outA.txt","w+");
-FILE *foutb=fopen("outB.txt","w+");
-FILE *blocks[20];
+FILE *blocks[numfiles];                                 //Array for auxillary created files
+FILE *result=fopen("result.txt","w");    //Out file
 
 //struct fo the array with length
 struct array{
     int* A=new int[deflen];
     int length;
     array(){
-        length=10;
+        length=deflen;
     }
 };
 
-FILE* create_default_file(const char* in){
-    string name="source";
-    FILE *test=fopen(in,"w+");
+//creating source files to merge
+FILE* create_default_file(){
+    string name="source_";
+    int curr_num=files_num;
+    string num=to_string(curr_num);
+    string txt=".txt";
+    num.append(txt);
+    name.append(num);
+    FILE *test=fopen(name.c_str(),"w+");
+    files_num++;
+    return test;
+}
+
+//func that delete all the source files
+void delete_files(){
+    for(int i=files_num-1;i>=0;i--){
+        string name="source_";
+        string num=to_string(--files_num);
+        name.append(num);
+        name.append(".txt");
+        fclose(blocks[i]);
+        remove(name.c_str());
+    }
 }
 
 //filling the file with random integers
 void fill_file(FILE* input){
-    for(int i=0;i<100;i++)
+    for(int i=0;i<rand_num;i++)
         fprintf(input,"%i%c",rand() % 1000,' ');
     rewind(input);
 }
@@ -37,13 +57,13 @@ int read_arr(FILE* input,array in){
     int temp;
     for(int i=0;i<deflen;i++) {
         if (feof(input)) {
-            return i-1;
+            return i;
             }
         fscanf(input,"%i",&temp);
         //A.push_back(temp);
         in.A[i]=temp;
     }
-    return 10;
+    return deflen;
 }
 
 //printing an array in file and on the screen
@@ -52,7 +72,7 @@ void print_arr(array in,FILE* out){
         cout<<in.A[i]<<" ";
         fprintf(out,"%d%c",in.A[i],' ');
     }
-    fprintf(out,"%c%c",'/',' ');
+    fprintf(out,"%c%c",' ','/');
     cout<<endl;
 }
 
@@ -119,37 +139,79 @@ void mergesort(array in,int left,int right){
     }
 }
 
-void make_sorted_blocks(FILE* in){
-    array temp;
-    while(!feof(in)){
-       temp.length=read_arr(in,temp);
-       print_arr(temp,fouta);
-    }
+//checking if file is empty(ends in '/')
+bool is_file_empty(FILE* fin){
+    char a;
+    long int pos=ftell(fin);//current position
+    //fscanf(fin,"%c",&a);
+    a=getc(fin);
+    while(a==' ')
+        a=getc(fin);
+    //cout<<" 1st:"<<a;
+    fseek(fin,pos,SEEK_SET);//return the pos
+    if(a=='/') return 1;
+    else return 0;
 }
 
+
 int main() {
-    string test;
-    int aaa=1232;
-    test=to_string(aaa);
-    cout<<test;
-    //create_default_file(test.c_str());
-
-
-    FILE *fina=fopen("inA.txt","r+");
-    FILE *finb=fopen("inB.txt","r+");
+    FILE *fina=fopen("inA.txt","w+");
 //filling files with the random integers
     fill_file(fina);
-    fill_file(finb);
 //creating the temp arr
     array arr;
+//creating source files,that include sorted blocks length of "deflen"
+    for(int i=0;i<numfiles;i++){
+        blocks[i]=create_default_file();
+        arr.length=read_arr(fina,arr);
+        mergesort(arr,0,arr.length);
+        print_arr(arr,blocks[i]);
+//making cursor on the start to operate with the block of data
+        rewind(blocks[i]);
+    }
+//actually MERGING *numfiles* IN ONE
+    //fprintf(blocks[0],"%i",22);
+    int smallest,smallest_pos,not_empty_pos,temp,k;
+    not_empty_pos=0;
+    int s=0;
+    while(s<rand_num){
+        while(is_file_empty(blocks[not_empty_pos])) {
+            not_empty_pos++;                            //finding the next NOT EMPTY file
+        }
+        if(not_empty_pos>19){                           //Unexpected behavior
+            cout<<"Unexpected error"<<endl;
+            return -1;
+        }
+        long int pos=ftell(blocks[not_empty_pos]);
+        fscanf(blocks[not_empty_pos],"%i",&smallest);   //making a new first element the smallest
+        fseek(blocks[not_empty_pos],pos,SEEK_SET);//return the pos in order not to miss the unused integer if the file
 
-    arr.length=read_arr(fina,arr);
-    print_arr(arr,fouta);
-    arr.length=read_arr(fina,arr);
-    print_arr(arr,foutb);
+        k=not_empty_pos;
+        while(k<numfiles) {                             //finding the smallest one
+            if(is_file_empty(blocks[k])){
+                k++;
 
-    mergesort(arr,0,arr.length);
-    print_arr(arr,foutb);
+            }
+            else {
+                pos = ftell(blocks[k]);
 
+                fscanf(blocks[k], "%i", &temp);
+                if (temp <= smallest) {
+                    smallest = temp;
+                    smallest_pos = k;
+                }
+
+                fseek(blocks[k], pos, SEEK_SET);
+                k++;
+            }
+        }
+        fscanf(blocks[smallest_pos],"%i",&temp);//just the easy way to step a cursor on one int right
+        fprintf(result,"%i%c",smallest,' ');
+    s++;
+    }
+    delete_files();
+
+    fclose(fina);
+    fclose(result);
     return 0;
 }
