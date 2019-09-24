757220bec93251615c2dfb545d0cb3d819769a68
Vitaliy Datsiuk <vetalvetal222@gmail.com>
2019-09-24 23:28:42 +0300

    Done the lab.
    Now the lab is OK. It would be nice if I added some documentary and comments to it, because it is a bit unobvious.

--- Bolts_and_nuts(2Lab)/main.cpp
+++ Bolts_and_nuts(2Lab)/main.cpp
@@ -13,6 +13,15 @@
 
 using namespace std;
 
+struct Border{
+    int left;
+    int right;
+    Border(){
+        left = 0;
+        right = quantity - 1;
+    }
+};
+
 void display(int Bolts[], int Nuts[]) {
     for (int i = 0; i < quantity; i++) {
         cout << "Bolt: " << Bolts[i] << ". Nut: " << Nuts[i] << endl;
@@ -31,26 +40,85 @@ void swap (int *in, int val_a, int val_b){
     in[val_b] = temp;
 }
 
-void shuffle(int *Bolts){
+void shuffle(int *in){
     for (int i = 0; i < 40; i++){
-        swap (Bolts,rand() % quantity, rand() % quantity);
+        swap (in,rand() % quantity, rand() % quantity);
     }
 
 }
 
-void determinator(int Bolts[], int Nuts[]){
-    
+Border detect(int *Bolts_0, int *Nuts, int val){
+    Border out;
+    for (int i = 0; i < quantity; i++){
+        if(Bolts_0[i] != 0){
+            if(Nuts[i] < val)
+                out.left = i + 1;
+            else {
+                out.right = i - 1;
+                return out;
+            }
+        }
+    }
+    return out;
+}
+
+void sorter(int *Bolts_0, int *Nuts, Border in, int val){
+
+    int length = in.right - in.left + 1;            //the length of the temp array
+    int Nuts_temp[length];
+
+    Border temp_bord;
+    temp_bord.left = 0;
+    temp_bord.right = length - 1;
+
+    for (int j = in.left; j < in.right + 1; j++){
+        if (val < Nuts[j]){
+            Nuts_temp[temp_bord.right] = Nuts[j];
+            temp_bord.right--;
+        }
+        if (val > Nuts[j]){
+            Nuts_temp[temp_bord.left] = Nuts[j];
+            temp_bord.left++;
+        }
+    }
+    Nuts_temp[temp_bord.left] = val;
+
+    for (int i = 0; i < length; i++){
+        Nuts[in.left + i] = Nuts_temp[i];
+    }
+    Bolts_0[temp_bord.left + in.left] = val;
+}
+
+void distributor(int *Bolts, int *Nuts){
+
+    int Bolts_0[quantity];
+
+    for(int j = 0; j < quantity; j++)
+        Bolts_0[j] = 0;
+
+    for (int i = 0; i < quantity; i++){
+        sorter(Bolts_0, Nuts, detect(Bolts_0, Nuts, Bolts[i]), Bolts[i]);
+    }
+
+    for (int k = 0; k < quantity; k++){
+        Bolts[k] = Bolts_0[k];
+    }
+
 }
 
 int main() {
+
     int Bolts[quantity], Nuts[quantity];
     for (int i = 0; i < quantity; i++){
-        Bolts[i] = Nuts[i] = i + 1;
+        Bolts[i] = Nuts[i] = i * 2 + 2;
     }
-    //display(Bolts, Nuts);
     shuffle(Bolts);
     shuffle(Nuts);
     display(Bolts, Nuts);
+
+    cout<<"---"<<endl;
+    distributor(Bolts,Nuts);
+    display(Bolts,Nuts);
 
     return 0;
 }
