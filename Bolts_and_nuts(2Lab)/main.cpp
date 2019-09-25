de956e6ab4ac4df217936d04c029969495e8d63b
Vitaliy Datsiuk <vetalvetal222@gmail.com>
2019-09-25 16:20:57 +0300

    Added documentary

--- Bolts_and_nuts(2Lab)/main.cpp
+++ Bolts_and_nuts(2Lab)/main.cpp
@@ -9,10 +9,12 @@
  */
 
 #include <iostream>
-#define quantity 50
+#define quantity 50                 //number of the nuts and bolts
 
 using namespace std;
 
+//structure which contain the left and the right index(used in sorting the part of the array
+//starting from left index and ending by right index)
 struct Border{
     int left;
     int right;
@@ -22,12 +24,14 @@ struct Border{
     }
 };
 
+//function that prints the bolt and the appropriate nut
 void display(int Bolts[], int Nuts[]) {
     for (int i = 0; i < quantity; i++) {
         cout << "Bolt: " << Bolts[i] << ". Nut: " << Nuts[i] << endl;
     }
 }
 
+//function that swaps two values in the input array
 void swap (int *in, int val_a, int val_b){
 
     if (val_a == val_b || val_a < 0 || val_b < 0 || val_a > quantity - 1 || val_b > quantity - 1){
@@ -40,6 +44,8 @@ void swap (int *in, int val_a, int val_b){
     in[val_b] = temp;
 }
 
+//function that randomly swaps the values in the array (the aim of this function in my program is to
+//make maximally random array to sort)
 void shuffle(int *in){
     for (int i = 0; i < 40; i++){
         swap (in,rand() % quantity, rand() % quantity);
@@ -47,6 +53,7 @@ void shuffle(int *in){
 
 }
 
+//this function detects the borders(left and right border) of the main array , that should be sorted
 Border detect(int *Bolts_0, int *Nuts, int val){
     Border out;
     for (int i = 0; i < quantity; i++){
@@ -62,6 +69,11 @@ Border detect(int *Bolts_0, int *Nuts, int val){
     return out;
 }
 
+//this fucntion sorts the part of array that determines by Border. Sorting is implemented like:
+//we take the first value from the border array and compare it with the input val. If it is bigger
+//we write it in the end of the array and decrement the right border, otherwise if it is smaller
+// we write it on the start of the array and increment the left border. The last part is to put the
+//input value on the needed place(which is basically empty)
 void sorter(int *Bolts_0, int *Nuts, Border in, int val){
 
     int length = in.right - in.left + 1;            //the length of the temp array
@@ -89,6 +101,9 @@ void sorter(int *Bolts_0, int *Nuts, Border in, int val){
     Bolts_0[temp_bord.left + in.left] = val;
 }
 
+//this function takes all the bolts and sort the nuts in correspondence to the bolt: either it is
+//bigger or smaller. Than it takes the next bolt and do the same, firtsly comparing it with the
+//already sorted nuts to determine the part of the array to sort
 void distributor(int *Bolts, int *Nuts){
 
     int Bolts_0[quantity];
@@ -110,14 +125,15 @@ int main() {
 
     int Bolts[quantity], Nuts[quantity];
     for (int i = 0; i < quantity; i++){
-        Bolts[i] = Nuts[i] = i * 2 + 2;
+        Bolts[i] = Nuts[i] = i * 2 + 2;         //You can fill the array whatever you like
     }
     shuffle(Bolts);
     shuffle(Nuts);
+
     display(Bolts, Nuts);
 
     cout<<"---"<<endl;
-    distributor(Bolts,Nuts);
+    distributor(Bolts,Nuts);                    //main function
     display(Bolts,Nuts);
 
     return 0;
